8e5f72a97db6be82ee4871d45baf36c0811466ec
xjhuang <xjhuang@s-an.org>
2017-10-27 17:44:37 +0800

    add new iot devices: dht11

--- Repository/DHT11.py
+++ Repository/DHT11.py
@@ -1,18 +1,44 @@
-import sys
 import socket
 
 class DHT11:
     temperature = ''
     humidity = ''
 
-    def __init__(self, deviceIp):
+    def __init__(self, deviceIp, deviceUuid, deviceName):
         self.ip = deviceIp
+        self.uuid = deviceUuid
+        self.name = deviceName
 
-    def getTemperaturen(self):
-        self.connectWithDevice('getTemp')
-    
-    def getHumidity(self):
-        self.connectWithDevice('getHumidity')
+    def getTempAndHumi(self):
+        recvdata = self.connectWithDevice('getTemp&Humi')
+        self.temperature, self.humidity = recvdata.split('&')
+
+    def getDeviceAttribute(self):
+        try:
+            self.getTempAndHumi()
+        except Exception:
+            return None
+
+        device = {}
+        device['uuid'] = self.uuid
+        device['name'] = self.name
+
+        deviceContent = []
+        deviceContent1 = {}
+        deviceContent1['type'] = 'text'
+        deviceContent1['name'] = 'Temperature'
+        deviceContent1['value'] = self.temperature + '℃'
+
+        deviceContent1 = {}
+        deviceContent1['type'] = 'text'
+        deviceContent1['name'] = 'Humidity'
+        deviceContent1['value'] = self.humidity + '%'
+        deviceContent.append(deviceContent1)
+
+
+        device['deviceContent'] = deviceContent
+        return device
+
 
     def connectWithDevice(self, cmd):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
